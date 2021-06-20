09e0728a129db2f3ff9432b7b22760082d79fad0
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-20 15:25:24 -0300

    adicionando o conteúdo do componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Header() {
-  return <div></div>;
+  return (
+    <HeaderWrapper>
+      <h1>Validação JS com Regex</h1>
+      <p>Este é um simples formulário com as validações feitas somente em regex.</p>
+      <p>Para liberar o botão de envio, atenda as seguintes condições no preenchimento:</p>
+      <ul>
+        <li>o nome de usuário deve ter SOMENTE letras e nenhum espaço</li>
+        <li>o email deve ser SOMENTE @gmail.com (pode ser um endereço fictício desde que seja um email válido)</li>
+        <li>a senha deve ter 5 letras maiúsculas, 6 símbolos e 2 hífens</li>
+      </ul>
+    </HeaderWrapper>
+  );
 }
 
 export default Header;
