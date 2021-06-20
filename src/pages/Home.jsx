a10fc396e9f0ea1cbb0c7f87a6cfff823db9b0a5
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-20 15:24:33 -0300

    refatorando a página Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function Home() {
-  return <div>olá</div>;
+  return (
+    <Container>
+      <Header />
+      <span>form</span>
+    </Container>
+  );
 }
 
 export default Home;
